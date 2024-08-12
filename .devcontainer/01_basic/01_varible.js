b5a408276224124f8b374d16d12438c2b755fc10
rawheat12 <rk3742816@gmail.com>
2024-08-12 22:45:56 +0000

    varialbe

---
+++ .devcontainer/01_basic/01_varible.js
@@ -0,0 +1,11 @@
+const accountId = "roh334" 
+// the constvariable can't change
+let accpuntEmail = "rohit@google.com"
+ // it can be change
+
+var accountPassword = "334422"
+ // var not use
+accountCity = "guwahati" 
+//javascript it assign as variable
+
+console.table([typeof accountId,typeof accountPassword,typeof accpuntEmail,typeof accountCity]);
