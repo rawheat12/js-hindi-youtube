c645f679065c29a2264da263ae847090331c9125
rawheat12 <rk3742816@gmail.com>
2024-08-25 13:42:52 +0000

    conversion_of_datatype

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+ let score = false
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+
+
+console.log(typeof valueInNumber);
+console.log( valueInNumber); 
+// "33"=> 33
+// "33abc"=> NaN
+// true => 1; false =>0
+
+let isLoggedIn = "rohit"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+//""=> false
+//"rohit"=> true
+
+let somNumber = 33
+let stringNumber = String(somNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
