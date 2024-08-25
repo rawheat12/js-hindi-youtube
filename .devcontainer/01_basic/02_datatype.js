7d544cc9dcb1601764f5dc87cf859b9872551abc
rawheat12 <rk3742816@gmail.com>
2024-08-25 13:17:20 +0000

    datatype

---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,27 @@
+"use stict"; // treat all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser
+console.log(3+ "3"); // code readbility should be high
+
+console.log("histesh")
+
+
+let name = "histesh"
+let age = 18
+let isLoggedIn = false
+
+// number =>2 2 to power 53
+// bigint
+// string => 
+    // boolean => true/flase
+// null => standalone value 
+// undefined =>
+    // symbol => unique
+
+
+// object
+
+console.log(typeof 3+ "3");
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
