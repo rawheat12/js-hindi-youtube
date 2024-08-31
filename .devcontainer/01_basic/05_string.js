f3f0b5ea01360cd7399e3726a5a4e92558e526c4
rawheat12 <rk3742816@gmail.com>
2024-08-31 16:40:42 +0000

    string

---
+++ .devcontainer/01_basic/05_string.js
@@ -0,0 +1,17 @@
+const name = "rohit"
+const repoCount = 1
+
+//console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} and my repo is ${repoCount}`);
+
+
+
+const GameName = new String('rohitkk')
+
+console.log(GameName[0]);
+console.log(GameName.indexOf('t'));
+
+
+
+
